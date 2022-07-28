be2af5bb40f9d9346120a5097530f1fa0622845a
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-07-27 21:55:05 -0300

    Adicionado API do google maps, falta apenas a chave

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const nomeDoCliente = document.querySelector("#nomeCliente");
+const pesoDaEntrega = document.querySelector("#pesoEntrega");
+
+const botaoFormularioCadastrar = document.querySelector(".cadastrarCliente");
+
+botaoFormularioCadastrar.addEventListener("click", () => {
+  let teste = nomeDoCliente.value;
+});
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
